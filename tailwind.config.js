1e9c11803b6a8572a481145d00d1e13a137457d7
fromarys <fromarys@gmail.com>
2022-11-06 14:48:59 +0600

    feat: add additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,7 @@ module.exports = {
       },
       backgroundImage: {
         mainGradient: 'linear-gradient(116.7deg, rgba(70, 129, 137, 0.1) 9.51%, rgba(7, 87, 98, 0.1) 95.18%)',
+        coverGradient: 'linear-gradient(116.7deg, #468189 9.51%, #075762 95.18%)',
       },
     },
   },
