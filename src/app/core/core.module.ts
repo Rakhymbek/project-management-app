b8b657a2856a9e34a8df7caf7c38f9e74cffdca1
Ranami <nasyrov.ramir@mail.ru>
2022-11-07 11:33:03 +0600

    feat: add click outside directive into core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, WelcomePageComponent, NotFoundPageComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    WelcomePageComponent,
+    NotFoundPageComponent,
+    ClickOutsideDirective,
+  ],
   imports: [CommonModule, SharedModule],
   exports: [WelcomePageComponent, HeaderComponent],
 })
