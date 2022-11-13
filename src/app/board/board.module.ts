ff62b08f308ab9ee156590d0efa69894b748580b
fromarys <fromarys@gmail.com>
2022-11-14 00:07:48 +0600

    feat: add board page component

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BoardPageComponent } from './pages/board-page/board-page.component';
 import { BoardRoutingModule } from './board-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [BoardPageComponent],
-  imports: [BoardRoutingModule],
+  imports: [BoardRoutingModule, SharedModule],
 })
 export class BoardModule {}
--- src/app/board/pages/board-page/board-page.component.ts
+++ src/app/board/pages/board-page/board-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, pluck } from 'rxjs';
+import { IColumn, IColumnsData } from 'src/app/core/models/board.model';
+import { BoardService } from '../../../core/services/board.service';
 
 @Component({
   selector: 'app-board-page',
@@ -6,5 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-page.component.scss'],
 })
 export class BoardPageComponent implements OnInit {
-  ngOnInit(): void {}
+  public $id: Observable<string> = this.activatedRoute.params.pipe(pluck('id'));
+
+  public columns: IColumn[] | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute, private boardService: BoardService) {}
+
+  ngOnInit(): void {
+    // this.$id.pipe(
+    //   map((id) => this.getColumns(id)),
+    //   switchMap(($columns) => forkJoin($columns)),
+    //   switchMap((columns) => columns.flat().map()))
+    // ).subscribe((columns) => {
+    // this.columns = columns;
+    // })
+  }
+
+  private getColumns(id: string) {
+    return this.boardService.getAllColumns(id).pipe(
+      map((columns) => {
+        const data: IColumnsData = {
+          boardId: id,
+          columns,
+        };
+        return data;
+      }),
+    );
+  }
+
+  private getTasks(boardId: string, columnId: string) {
+    return this.boardService.getAllTasks(boardId, columnId);
+  }
 }
