2c8848cb7108d3ce810b1d6bbcb5c63860db7260
fromarys <fromarys@gmail.com>
2022-11-15 13:07:00 +0600

    fix: fix dnd

--- src/app/board/pages/board-page/board-page.component.ts
+++ src/app/board/pages/board-page/board-page.component.ts
@@ -1,14 +1,8 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, mergeMap, Observable, of, pluck } from 'rxjs';
-import {
-  ITask,
-  IColumnInfo,
-  IColumn,
-  IBoardData,
-  ITaskData,
-} from 'src/app/core/models/board.model';
+import { map, mergeMap, Observable, pluck } from 'rxjs';
+import { ITask, IBoardData, ITaskData } from 'src/app/core/models/board.model';
 import { BoardService } from '../../../core/services/board.service';
 
 @Component({
@@ -28,14 +22,6 @@ export class BoardPageComponent implements OnInit {
   ngOnInit(): void {
     this.$id
       .pipe(
-        // mergeMap((id) => {
-        //   this.id = id;
-        //   return this.getColumns(id);
-        // }),
-        // map((columns) => {
-        //   return columns.map((column) => this.getTasks(this.id!, column));
-        // }),
-        // mergeMap(($columns) => forkJoin($columns)),
         mergeMap((id) => {
           this.id = id;
           return this.getBoard(id);
@@ -49,25 +35,6 @@ export class BoardPageComponent implements OnInit {
 
   private getBoard(id: string) {
     return this.boardService.getBoard(id);
-  }
-
-  private getColumns(id: string): Observable<IColumn[]> {
-    return this.boardService.getAllColumns(id);
-  }
-
-  private getTasks(boardId: string, column: IColumn): Observable<IColumnInfo> {
-    return this.boardService.getAllTasks(boardId, column.id).pipe(
-      mergeMap((tasks) => {
-        const data: IColumnInfo = {
-          boardId,
-          columnId: column.id,
-          title: column.title,
-          order: column.order,
-          tasks,
-        };
-        return of(data);
-      }),
-    );
   }
 
   protected getUserName(task: ITask): Observable<ITask> {
