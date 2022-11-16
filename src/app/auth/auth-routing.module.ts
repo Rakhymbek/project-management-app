7f7d2c8e47595dd809074696697173ade276ab95
Rakhymbek <sagrakha@gmail.com>
2022-11-16 12:32:26 +0600

    feat: add edit-user route and markup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { EditUserComponent } from './pages/edit-user/edit-user.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +13,11 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'edit-user',
+    component: EditUserComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
--- src/app/auth/pages/edit-user/edit-user.component.ts
+++ src/app/auth/pages/edit-user/edit-user.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidationService } from '../../services/validation.service';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.scss'],
 })
-export class EditUserComponent implements OnInit {
-  constructor() {}
+export class EditUserComponent {
+  hidePassword = true;
 
-  ngOnInit(): void {}
+  constructor(private validator: ValidationService, public authService: AuthService) {}
+
+  editUserForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    login: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, this.validator.passwordValidator()]),
+  });
+
+  get name() {
+    return this.editUserForm.get('name');
+  }
+
+  get login() {
+    return this.editUserForm.get('login');
+  }
+
+  get password() {
+    return this.editUserForm.get('password');
+  }
 }
