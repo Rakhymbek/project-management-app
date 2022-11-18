e2aec17e3bb94820c4311e99741e1a4d70ca8381
Yevgeniy Mineyev <fromarys@gmail.com>
2022-11-18 17:24:31 +0600

    feat: add column edition functionality

--- src/app/board/components/column/column.component.ts
+++ src/app/board/components/column/column.component.ts
@@ -1,5 +1,6 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { IColumnData, ITaskData } from 'src/app/core/models/board.model';
 import { EDialogEvents } from 'src/app/core/models/enums';
@@ -13,15 +14,25 @@ import { BoardService } from 'src/app/core/services/board.service';
 export class ColumnComponent implements OnInit {
   @Input() column: IColumnData | undefined;
 
-  @Input() id: string | undefined;
+  @Input() boardId: string | undefined;
 
   @Output() deleteColumn: EventEmitter<string> = new EventEmitter();
 
   @Output() editColumn: EventEmitter<string> = new EventEmitter();
 
+  public editing: boolean = false;
+
+  public title = new FormControl(``);
+
   constructor(private activatedRoute: ActivatedRoute, private boardService: BoardService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.title.setValue(this.column?.title || '');
+  }
+
+  get columnTitle(): string | null {
+    return this.title.value;
+  }
 
   protected dropTask(event: CdkDragDrop<ITaskData[] | undefined>): void {
     if (event.container.data && event.previousContainer.data) {
@@ -46,23 +57,34 @@ export class ColumnComponent implements OnInit {
       const task = event.container.data[event.currentIndex];
       const prevColumnId = event.previousContainer.id;
       const columnId = event.container.id;
-      if (this.id) {
+      if (this.boardId) {
         const body = {
           title: task.title,
           description: task.description,
           order: event.currentIndex + 1,
           userId: task.userId,
-          boardId: this.id,
+          boardId: this.boardId,
           columnId,
         };
-        this.boardService.updateTask(this.id, prevColumnId, task.id, body).subscribe();
+        this.boardService.updateTask(this.boardId, prevColumnId, task.id, body).subscribe();
       }
     }
   }
 
   protected openDialog(event: string) {
     if (event === EDialogEvents.delete) {
-      this.deleteColumn.emit(this.column!.id);
+      this.deleteColumn.emit(this.column?.id);
+    }
+  }
+
+  protected editColumnTitle(): void {
+    if (this.boardId && this.column?.id && this.columnTitle) {
+      this.boardService
+        .updateColumn(this.boardId, this.column?.id, {
+          title: this.columnTitle,
+          order: this.column.order,
+        })
+        .subscribe();
     }
   }
 }
