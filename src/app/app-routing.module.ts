c463d587b77e1d1f99413da27c116b3c5b158bec
Rakhymbek <sagrakha@gmail.com>
2022-11-25 22:24:43 +0600

    feat: add UnuthorizedGuard to welcome,signUp,login routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
+import { UnauthorizedGuard } from './core/guards/unauthorized.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'welcome',
     component: WelcomePageComponent,
+    canActivate: [UnauthorizedGuard],
   },
   {
     path: 'auth',
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,15 +4,18 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { EditUserComponent } from './pages/edit-user/edit-user.component';
 import { AuthGuard } from '../core/guards/auth.guard';
+import { UnauthorizedGuard } from '../core/guards/unauthorized.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: SignUpComponent,
+    canActivate: [UnauthorizedGuard],
   },
   {
     path: 'login',
     component: LoginPageComponent,
+    canActivate: [UnauthorizedGuard],
   },
   {
     path: 'edit-user',
